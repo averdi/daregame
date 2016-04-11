1a1bdc3420a58a9bb3949ba1bba6ce5e4bf7b203
Jason Horine <outbackdog@gmail.com>
2016-04-10 20:57:39 -0500

    Starter code added for Instagram log in.  Still need to handle log-in as a redirect for clicking a restricted area and add log in/out to header.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request'); // for the http calls
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// route for Instagram sign-in
+router.get('/sign-in', function(req, res){
+  var id = process.env.INSTAGRAM_ID;
+  var url = 'http://localhost:3000/auth';//the environment variable
+  //redirect the browser to this address, passing it INSTAGRAM_ID for 'client_id'.
+  //have it send the response back to /auth
+  res.redirect(`https://api.instagram.com/oauth/authorize/?client_id=${id}&redirect_uri=${url}&response_type=code`)
+})
+
+// route to recieve the request token and continue to log in with Instagram
+router.get('/auth', function(req, res, next){
+  var body = { // create body for the request
+    client_id: process.env.INSTAGRAM_ID,
+    client_secret: process.env.INSTAGRAM_SECRET,
+    grant_type: 'authorization_code',
+    redirect_uri: 'http://localhost:3000/auth',
+    code: req.query.code // the code variable from the URI returned by Instagram
+  };
+
+console.log("req.query.code= " + req.query.code); // I want to see it
+  var options = {
+    url: 'https://api.instagram.com/oauth/access_token',
+    method: 'POST',
+    body: querystring.stringify(body)// turn body into a string
+  };
+
+request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) { // if no error and status is good,
+      console.log(body); // show body
+      var name = JSON.parse(body).user.username; // put name in a variable
+      var photo = JSON.parse(body).user.profile_picture;// put profile pic in a variable
+      // show result, passing the user and image to the view
+      res.render('welcome', {user: name, image: photo});
+    }
+  });
+})
 
 module.exports = router;
