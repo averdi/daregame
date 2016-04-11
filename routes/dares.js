2ceef91bcedea068db2c2c7fd90271b56b566254
Antonia Verdi <averdi@gm.slc.edu>
2016-04-11 11:38:00 -0500

    small changes

--- routes/dares.js
+++ routes/dares.js
@@ -5,8 +5,8 @@ var Dare = require('../models/dare');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-var filter = { category: {$in: [req.body.category]}}
+console.log(req.body.category);
+var filter = { category: {$in: ['req.body.category']}}
 
   Dare.findOneRandom(filter, function(err, dare) {
     res.render('dare', {
