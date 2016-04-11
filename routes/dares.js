049aa70f0bc2d298201ed4304eca9f8518535478
Jason Horine <outbackdog@gmail.com>
2016-04-11 11:35:00 -0500

    changed index form buttons' method from get to post, and did the same in dares route to get those working.

--- routes/dares.js
+++ routes/dares.js
@@ -4,8 +4,8 @@ var router = express.Router();
 var Dare = require('../models/dare');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-
+router.post('/', function(req, res, next) {
+console.log(req.body);
 var filter = { category: {$in: [req.body.category]}}
 
   Dare.findOneRandom(filter, function(err, dare) {
