a37a79f55c9e96c3058853700f2c76f93c172ce3
Jason Horine <outbackdog@gmail.com>
2016-04-10 21:09:21 -0500

    Instagram is signing in.  It redirects to a welcome page.  Going to work on getting it to handle the log in more intelligently.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request'); // for the http calls
+var querystring = require('querystring');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
