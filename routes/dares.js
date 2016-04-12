bfc96ecb97eb1245b8dc20531f70467313d35bf3
Antonia Verdi <averdi@gm.slc.edu>
2016-04-12 10:31:39 -0500

    added normalize.css

--- routes/dares.js
+++ routes/dares.js
@@ -6,15 +6,39 @@ var User = require('../models/user');
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-var filter = { category: {$in: [req.body.category]}}
+  var filter = { category: {$in: [req.body.category]}};
 
-  Dare.findOneRandom(filter, function(err, dare) {
-    res.render('dare', {
-      title: 'Dares',
-      dare: dare
-       });
-    });
+  User.findOne('instagramID', '_id', function(userErr, currentUser) {
+    if (userErr) console.log(userErr);
+    console.log('users=' + currentUser);
+
+    Dare.findOneRandom(filter, function(dareErr, dare) {
+      res.render('dare', {
+        title: 'Dares',
+        dare: dare,
+        user: currentUser
+         });
+      });
+  })
+
+
 });
+
+router.get('/api/random', function(req, res, next) {
+  var filter = { category: {$in: [req.query.category]}};
+
+  User.findOne('instagramID', '_id', function(userErr, currentUser) {
+    if (userErr) console.log(userErr);
+    console.log('users=' + currentUser);
+
+    Dare.findOneRandom(filter, function(dareErr, dare) {
+      if (dareErr) console.log(dareErr);
+
+      res.json(dare);
+    });
+  })
+});
+
 
 
 module.exports = router;
